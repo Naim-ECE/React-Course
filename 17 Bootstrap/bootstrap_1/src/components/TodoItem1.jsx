b149adf505bf24e40d464b9b7a7880d8fde63009
Md. Naimur Rahman Naim <naimur2210026@gmail.com>
2025-07-09 09:00:33 +0600

    prettifying the code

--- 17 Bootstrap/bootstrap_1/src/components/TodoItem1.jsx
+++ 17 Bootstrap/bootstrap_1/src/components/TodoItem1.jsx
@@ -1,27 +1,22 @@
 function TodoItem1() {
+  let todoName = "Buy Milk";
+  let todoDate = "04/12/2024";
 
-    let todoName = "Buy Milk";
-    let todoDate = "04/12/2024";
-
-    return (
-        <>
-            <div className="container">
-
-
-                <div className="row gap-row">
-                    <div className="col-4">
-                        {todoName}
-                    </div>
-                    <div className="col-4">
-                        {todoDate}
-                    </div>
-                    <div className="col-2 d-flex justify-content-center">
-                        <button type="button" className="btn btn-danger kg-button">Delete</button>
-                    </div>
-                </div>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className="container">
+        <div className="row gap-row">
+          <div className="col-4">{todoName}</div>
+          <div className="col-4">{todoDate}</div>
+          <div className="col-2 d-flex justify-content-center">
+            <button type="button" className="btn btn-danger kg-button">
+              Delete
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default TodoItem1;
