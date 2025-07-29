2683e083f7ac8dcc13f9e72270d764acc4abe98d
Md. Naimur Rahman Naim <naimur2210026@gmail.com>
2025-07-29 22:58:25 +0600

    tnt

--- Calculator/Calculator-1/src/components/Input.jsx
+++ Calculator/Calculator-1/src/components/Input.jsx
@@ -1,11 +1,16 @@
 import styles from "./Input.module.css";
 
-const Input = ({displayValue}) => {
+const Input = ({ displayValue }) => {
   return (
     <>
-      <input className={styles.display} type="text" value={displayValue} readOnly/>
+      <input
+        className={styles.display}
+        type="text"
+        value={displayValue}
+        readOnly
+      />
     </>
   );
-};
+}; 
 
 export default Input;
