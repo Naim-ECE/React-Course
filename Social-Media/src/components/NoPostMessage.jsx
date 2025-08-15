07d3edba1f237e1f72565b2224cb44cb3fa9f2fe
Md. Naimur Rahman Naim <naimur2210026@gmail.com>
2025-08-15 20:47:43 +0600

    added np post available message

---
+++ Social-Media/src/components/NoPostMessage.jsx
@@ -0,0 +1,10 @@
+import style from "../components/NoPostMessage.module.css"
+const NoPostMessage = () => {
+  return (
+    <>
+      <h2 className={`${style.editheader}`}>No Post Available</h2>
+    </>
+  );
+};
+
+export default NoPostMessage;
--- Social-Media/src/components/PostList.jsx
+++ Social-Media/src/components/PostList.jsx
@@ -1,9 +1,19 @@
 import { useContext } from "react";
 import Post from "./Post";
 import { PostList as PostListData } from "../store/post-list-store";
+import NoPostMessage from "./NoPostMessage";
 
 const PostList = () => {
   const { postList } = useContext(PostListData);
+
+  if (postList.length === 0) {
+    return (
+      <>
+        <NoPostMessage />
+      </>
+    );
+  }
+
   return (
     <>
       {postList.map((post) => {
